c7711fb6a41723cc6ab0e15988d30620a41f7d7f
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-11-18 09:25:32 -0500

    added game logic - unit 8

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
-var cardOne = "queen"
 
-var cardTwo = "queen"
+// global variables
 
-var cardThree = "king"
- 
-var cardFour = "king"
+var cards = [ "queen", "queen", "king", "king"]
 
-console.log("user flipped " + cardOne + " and " + cardFour) 
+var cardOne = cards[0]
+
+var cardTwo = cards[1]
+
+var cardsInPlay = [ ]
+
+// add cards to cardsInPlay
+
+cardsInPlay.push(cardOne, cardTwo)
+
+// check if 2 cards were flipped
+
+if ( cardsInPlay.length === 2 ) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!")
+
+    } else {
+        alert("Sorry, try again.")
+
+    }
+
+}
+
+// alert user
+
+console.log("user flipped " + cardOne + " and " + cardTwo) 
