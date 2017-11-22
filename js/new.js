01c714ce3b309e9f7427fb7eaec985104938c363
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-11-22 10:30:09 -0500

    working game

--- js/new.js
+++ js/new.js
@@ -34,7 +34,7 @@ var checkMatch = function() {
         // Check for length and match
 
         if (cardsInPlay.length === 2) {
-            if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+            if (cardsInPlay[0] === cardsInPlay[1]) {
                 alert('You found a match!')
             } 
             else {
@@ -45,22 +45,49 @@ var checkMatch = function() {
 
 // (unit 9) ONE FUNCTION TO SELECT/FLIP CARDS
 
-var flipcard = function(cardId) {
+var flipcard = function() {
+    // get data Id of flipped card
+    var cardId = this.getAttribute('data-id');
 
     console.log('User flipped ' + cards[cardId].rank + ' of ' + cards[cardId].suit)
 
     // Add flipped cards to cardsInPlay
 
-    cardsInPlay.push(cards[cardId])
+    cardsInPlay.push(cards[cardId].rank)
 
-// new if statement:
+    // flip face of card
+    this.setAttribute('src', cards[cardId].cardImage)
+    // new if statement:
     if (cardsInPlay.length === 2) {
         checkMatch()
     }
 }
 
-flipcard(3)
-flipcard(2)
+// create game board
+
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+        var newCard = document.createElement('img')
+        newCard.setAttribute('src', 'images/back.png')
+        newCard.setAttribute('data-id', i)
+        // add event listener and tie flipcard() function
+        newCard.addEventListener('click', flipcard)
+        // grab board
+        var board = document.getElementById('game-board')
+        // add card to board
+        board.appendChild(newCard)
+
+    }
+
+}
+
+
+createBoard()
+
+
+
+// flipcard(3)
+// flipcard(2)
 
 // ALERT USER OF RESULTS:
 
